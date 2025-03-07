921e202374c7931321e84568de6b1e69e747c03c
habo3hnk <habo3hnk>
2025-03-07 19:27:49 +0000

    Select multiple

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This script adds the ability to use `rmpc` (a client for controlling the MPD music player) in the `yazi` file manager. With it, you can open and play music files directly from `yazi`, adding them to the MPD playlist.
 
+![example](media/example.gif)
+
 **Key Features:**
 - Opening and playing music files via `rmpc`.
 - Adding selected tracks to the MPD playlist.
--- yazi_rmpc_opener.py
+++ yazi_rmpc_opener.py
@@ -4,30 +4,36 @@ import subprocess
 import sys
 import json
 
-def run_rmpc_command(audio_name):
-    try:
-        status_json = subprocess.run(
-            ['rmpc', "status"],
-            text=True,
-            capture_output=True
-        )
-        status = json.loads(status_json.stdout)
-        subprocess.run(['rmpc', "add", audio_name])
-        subprocess.run(['rmpc', "play"])
- 
-        if status["state"] != "Stop":
-            subprocess.run(['rmpc', "next"])
-        subprocess.run(['rmpc'])
 
-    except FileNotFoundError:
-        print("Error: rmpc is not installed.")
+def get_rmpc_status():
+    status_json = subprocess.run(
+        ['rmpc', "status"],
+        text=True,
+        capture_output=True
+    )
+    status = json.loads(status_json.stdout)
+    return status
+
+
+def add_track_to_queue(track, rmpc_status):
+    subprocess.run(['rmpc', "add", track])
+
+    if rmpc_status["state"] != "Stop":
+        subprocess.run(['rmpc', "next"])
 
 
 def main():
-    if len(sys.argv) == 2:
-        run_rmpc_command(sys.argv[1])
-    else:
+    if len(sys.argv) < 2:
         print("No argv")
+        sys.exit(0)
+
+    rmpc_status = get_rmpc_status()
+
+    for track in sys.argv[1:]:
+        add_track_to_queue(track, rmpc_status)
+
+    subprocess.run(['rmpc', "play"])
+    subprocess.run(['rmpc'])
 
 if __name__ == "__main__":
     main()
