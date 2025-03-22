00bb8b689255a12c6f4cb9e29e98289c0d74d524
habo3hnk <habo3hnk>
2025-03-22 02:42:22 +0000

    refactor

--- yazi_rmpc_opener.py
+++ yazi_rmpc_opener.py
@@ -3,44 +3,75 @@
 import subprocess
 import sys
 import json
+import logging
+import argparse
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def get_rmpc_status():
-    status_json = subprocess.run(
-        ['rmpc', "status"],
-        text=True,
-        capture_output=True
-    )
-    status = json.loads(status_json.stdout)
-    return status
+    try:
+        status_json = subprocess.run(
+            ["rmpc", "status"], text=True, capture_output=True, check=True
+        )
+        status = json.loads(status_json.stdout)
+        return status
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error getting rmpc status: {e}")
+        sys.exit(1)
+    except json.JSONDecodeError as e:
+        logging.error(f"Error decoding JSON from rmpc status: {e}")
+        sys.exit(1)
 
 
 def select_last_added_track(playlist_len, current_track_number):
-    for i in range(playlist_len - current_track_number):
-        subprocess.run(['rmpc', "next"])
+    for _ in range(playlist_len - current_track_number):
+        try:
+            subprocess.run(["rmpc", "next"], check=True)
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error selecting next track: {e}")
+            sys.exit(1)
 
 
 def add_tracks_to_queue(tracks, rmpc_status):
-    for i in tracks:
-        subprocess.run(['rmpc', "add", i])
+    for track in tracks:
+        try:
+            subprocess.run(["rmpc", "add", track], check=True)
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error adding track {track} to queue: {e}")
+            sys.exit(1)
 
-    if rmpc_status["state"] != "Stop" and len(tracks)==1:
+    if rmpc_status["state"] != "Stop" and len(tracks) == 1:
         playlist_len = int(rmpc_status["playlistlength"])
         current_track_number = int(rmpc_status["song"])
         select_last_added_track(playlist_len, current_track_number)
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Add tracks to the rmpc queue and play them."
+    )
+    parser.add_argument("tracks", nargs="+", help="List of tracks to add to the queue.")
+    return parser.parse_args()
+
+
 def main():
-    if len(sys.argv) < 2:
-        print("No argv")
-        sys.exit(0)
+    args = parse_arguments()
 
     rmpc_status = get_rmpc_status()
 
-    add_tracks_to_queue(sys.argv[1:], rmpc_status)
+    add_tracks_to_queue(args.tracks, rmpc_status)
 
-    subprocess.run(['rmpc', "play"])
-    subprocess.run(['rmpc'])
+    try:
+        subprocess.run(["rmpc", "play"], check=True)
+        subprocess.run(["rmpc"], check=True)
+
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Error playing tracks: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
